8fead65d655ed86a68fc6ae6ac1e00ab43b7a82e
Ashish <akarhade5@gmail.com>
2022-06-26 09:54:51 +0530

    basic functionalities - add, complete, delete, list are read

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -14,6 +14,9 @@ const (
 
 func main() {
 	add := flag.Bool("add", false, "add a new todo")
+	complete := flag.Int("complete", 0, "mark a todo as completed task")
+	delete := flag.Int("delete", 0, "deletes a todo task")
+	list := flag.Bool("list", false, "list all todos")
 
 	flag.Parse()
 
@@ -32,6 +35,34 @@ func main() {
 			fmt.Fprintln(os.Stderr, err.Error())
 			os.Exit(1)
 		}
+
+	case *complete > 0:
+		err := todos.Complete(*complete)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+		err = todos.Store(todoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+
+	case *delete > 0:
+		err := todos.Delete(*delete)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+		err = todos.Store(todoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+
+	case *list:
+		todos.Print()
+
 	default:
 		fmt.Fprintln(os.Stdout, "invalid command")
 		os.Exit(0)
--- todo.go
+++ todo.go
@@ -3,6 +3,7 @@ package todo
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"time"
@@ -79,4 +80,11 @@ func (t *Todos) Store(filename string) error {
 		return err
 	}
 	return ioutil.WriteFile(filename, data, 0644)
+}
+
+func (t *Todos) Print() {
+	for i, item := range *t {
+		i++
+		fmt.Printf("%d - %s\n", i, item.Task)
+	}
 }
